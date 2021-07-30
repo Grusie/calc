1028b2e5cc6cb2d4ce95a50828493b018b08c709
Guest <guest-l7hkb3@h28968.sgb.skir.kr>
2021-07-30 15:08:37 +0900

    test-2

--- calc.js
+++ calc.js
@@ -1,1 +1,26 @@
-console.log("AS");
+// function input_data()
+// {
+//     let input_btn=document.getElementsByClassName("btn btn-outline-primary");
+//     let result_div=document.getElementById("result");
+//     for(let i=0; i<input_btn.length; i++)
+//     {
+//         console.log(input_btn[i]);
+//         result_div.innerHTML+=String(input_btn[i].value);
+//     }
+// }
+function input_data()
+{
+    var btn = document.querySelectorAll('.btn');
+    var result_div = document.getElementById("result");
+
+    [].forEach.call(btn, function(e){ 
+    e.addEventListener("click", function(){
+        var index=getElementIndex(btn, e);
+        result_div.innerHTML += btn[index].value
+    }, false); 
+    });
+}
+function getElementIndex(e, range) {
+  if (range) return [].indexOf.call(e, range);
+  return [].indexOf.call(e.parentNode.children,e.parentNode);
+}
