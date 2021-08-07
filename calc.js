cf939a2fc39b11aad89a9fe2c316efeb6dc04b4c
Guest <guest-qxjx0z@h28952.sgb.skir.kr>
2021-08-07 23:33:22 +0900

    test-3

--- calc.js
+++ calc.js
@@ -8,19 +8,50 @@
 //         result_div.innerHTML+=String(input_btn[i].value);
 //     }
 // }
-function input_data()
-{
-    var btn = document.querySelectorAll('.btn');
-    var result_div = document.getElementById("result");
 
-    [].forEach.call(btn, function(e){ 
-    e.addEventListener("click", function(){
-        var index=getElementIndex(btn, e);
+function input_data(){
+    const btn = document.querySelectorAll('.btn');
+    const result_div = document.getElementById("result");
+
+    btn.forEach(function(e){
+       e.addEventListener("click", function(){
+        let index = getElementIndex(btn, e);
+        calc(e);
         result_div.innerHTML += btn[index].value
-    }, false); 
-    });
-}
+      })
+    })
+  }
+
 function getElementIndex(e, range) {
-  if (range) return [].indexOf.call(e, range);
-  return [].indexOf.call(e.parentNode.children,e.parentNode);
+  if (!!range) {
+    return [].indexOf.call(e, range);
+  }
+}
+
+function check_op(e){
+  if(isNaN(Number(e.value))) return [e.value, "oper"];
+  else return [e.value];
+}
+function calc(e){
+  
+  let Arr_val = check_op(e);
+  console.log(Arr_val[0]);
+  if(Arr_val[1]){
+    switch(Arr_val[0]){
+      case "+" : break;
+      case "-" : break;
+      case "×" : break;
+      case "÷" : break;
+      case "%" : break;
+      case "=" : break;
+      case "←" : break;
+      case "." : break;
+      case "x²" : break;
+      case "√" : break;
+      case "1/x" : break;
+      case "CE" : break;
+      case "C" : break;
+      case "±" : break;
+    }
+  } 
 }
