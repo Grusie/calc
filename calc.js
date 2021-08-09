27b97ecd61fa32488f8336aa601f683a656fe95b
Guest <guest-x4znh6@h28948.sgb.skir.kr>
2021-08-09 15:20:26 +0900

    test-4

--- calc.js
+++ calc.js
@@ -1,57 +1,52 @@
-// function input_data()
-// {
-//     let input_btn=document.getElementsByClassName("btn btn-outline-primary");
-//     let result_div=document.getElementById("result");
-//     for(let i=0; i<input_btn.length; i++)
-//     {
-//         console.log(input_btn[i]);
-//         result_div.innerHTML+=String(input_btn[i].value);
-//     }
-// }
+//let queue = [];
 
 function input_data(){
     const btn = document.querySelectorAll('.btn');
     const result_div = document.getElementById("result");
-
+    
     btn.forEach(function(e){
        e.addEventListener("click", function(){
-        let index = getElementIndex(btn, e);
-        calc(e);
-        result_div.innerHTML += btn[index].value
+        calc(e, result_div);
+        result_div.innerHTML+=e.value;
+        //console.log([].indexOf.call(btn,e));
       })
     })
   }
 
-function getElementIndex(e, range) {
-  if (!!range) {
-    return [].indexOf.call(e, range);
+function calc(e, result){
+//  console.log(e.value);
+  let temp = result.innerHTML;
+  switch(e.value){
+    case "+" : 
+      getNum2();
+      Number(temp)
+      console.log(temp);
+      break;
+    case "-" : break;
+    case "×" : break;
+    case "÷" : break;
+    case "%" : break;
+    case "=" : break;
+    case "←" : break;
+    case "." : break;
+    case "x²" : break;
+    case "√" : break;
+    case "1/x" : break;
+    case "CE" : break;
+    case "C" : break;
+    case "±" : break;
   }
 }
 
-function check_op(e){
-  if(isNaN(Number(e.value))) return [e.value, "oper"];
-  else return [e.value];
-}
-function calc(e){
+function getNum2(){
+  do
+  {const btn = document.querySelectorAll('.btn');
+  const result_div = document.getElementById("result");
   
-  let Arr_val = check_op(e);
-  console.log(Arr_val[0]);
-  if(Arr_val[1]){
-    switch(Arr_val[0]){
-      case "+" : break;
-      case "-" : break;
-      case "×" : break;
-      case "÷" : break;
-      case "%" : break;
-      case "=" : break;
-      case "←" : break;
-      case "." : break;
-      case "x²" : break;
-      case "√" : break;
-      case "1/x" : break;
-      case "CE" : break;
-      case "C" : break;
-      case "±" : break;
-    }
-  } 
+  btn.forEach(function(e){
+    e.click()
+    e.click("click", function(){
+      result_div.innerHTML+=e.value;
+    })
+  })} while(false);
 }
